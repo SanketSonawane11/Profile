53f1bc87595d30cf286813161eb008c463131ff5
SanketSonawane11 <sanketsonawane424@gmail.com>
2023-12-08 14:41:02 +0530

    Re design

--- src/app/page.js
+++ src/app/page.js
@@ -12,35 +12,25 @@ function About() {
     Element.scrollIntoView({ behaviour: 'smooth'});
   };
 
-  useEffect( ()=>
-  {
-
-    const t = gsap.Timeline();
-
-    t.from(".pageOne", {backgroundColor: 'black', duration: 1})
-    .to(".pageOne", {backgroundColor: 'transparent', duration: 10})
-
-    .from(".nameText", {color: 'white', x: -1000, duration: 1})
-    .to(".nameText", {color: 'black', x:0,  duration: 4})
-
-    .to(".subText", {y: '3vw', duration: 4});
-
-  }, []);
+  useEffect(() => {
+    gsap.to('.main', {delay: 1, duration: 1.8, y: '-100%', scale: 0.959, borderBottomLeftRadius: '4vw', borderBottomRightRadius: '4vw', ease: "expo.out"});
+      }, []);
 
 
 
   return (
     
-    <div className='main'>
+    <div className='mainBody'>
 
-      <div className='pageOne'>
+      <div className='main'>
 
-        <div className='nameText'>
-        <h1 className='mainText'>Sanket Sonawane's</h1>
-        <h1 className='subText'>Portfolio</h1>
-        </div>
+        <h1 className='name'> Sanket Sonawane's</h1>
+
+        <h2 className='subText'> Portfolio </h2>
 
       </div>
+
+      
 
     </div>
 
