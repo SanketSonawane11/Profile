52cdfa0cd94e2eaa09fc5a865deb3c8872e5c057
SanketSonawane11 <sanketsonawane424@gmail.com>
2023-12-09 01:42:06 +0530

    Greeting animations and greeting page completed

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,10 @@
 'use client'
 
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import gsap from 'gsap';
 import Navbar from './Components/Navbar';
+// import $ from 'jquery';
+// import "textillate";
 
 
 
@@ -14,55 +16,218 @@ function About() {
     Element.scrollIntoView({ behaviour: 'smooth'});
   };
 
-  // useEffect(() => {
+  useEffect(() => {
   //   // const t = gsap.timeline();
 
   //   // t
-  //     gsap.from(".name", {duration: 1, opacity: 0, y: '-100%' })
+      gsap.from(".name", {duration: 1, opacity: 0, y: '-100%' })
 
-  //     gsap.to('.name', {duration: 1, opacity: 1, y: 0, ease: "back.out(1.7)" })
+      gsap.to('.name', {duration: 1, opacity: 1, y: 0, ease: "back.out(1.7)" })
 
-  //     gsap.from(".subText", {duration: 1, opacity: 0, y: '-100%'})
+      gsap.from(".subText", {duration: 1, opacity: 0, y: '-100%'})
 
-  //     gsap.to('.subText', {duration: 1, opacity: 1, y: 0, ease: "back.out(1.7)"})
+      gsap.to('.subText', {duration: 1, opacity: 1, y: 0, ease: "back.out(1.7)"})
 
-  //     gsap.to('.main', {delay: 2.3, duration: 1.8, y: '-100%', scale: 0.959, borderBottomLeftRadius: '40%', borderBottomRightRadius: '40%', ease: "expo.out"});
-  //     }, []);
+      // gsap.from(".english", {
+      //   // onStart: function () {
+      //   //   $(".english").textillate({
+      //   //     in: {
+      //   //       effect: "fadeInLeft",
+      //   //       callback: function () {
+      //   //         gsap.to(".english", {
+      //   //           duration: 1,
+      //   //           onComplete: function () {
+      //   //             $(".english").textillate("out");
+      //   //           },
+      //   //         });
+      //   //       },
+      //   //     },
+      //   //     out: {
+      //   //       effect: "fadeInRight",
+      //   //     },
+      //   //   });
+      //   // },
+      //   duration: 1,
+      //   opacity: 0,
+      //   x: '-10%',
+      //   ease: "power3.out",
+      // });
+
+      // gsap.to('.english', {
+      //   duration: 0.5,
+      //   x: 0,
+      //   opacity: 1,
+      //   ease: "power3.out",
+      // }, '+=1');
+
+      // gsap.to('.english', {
+      //   duration: 0.5,
+      //   opacity: 0,
+      //   x: '10%',
+      // }, '+=1');
+
+      // English text
+
+      gsap.from(".english", {
+        duration: 0.5,
+        opacity: 0,
+        x: '-20%',
+        ease: "power3.out",
+      });
+      
+      gsap.to('.english', {
+        duration: 0.5,
+        opacity: 1,
+        x: 0, 
+        ease: "power3.out",
+        delay: 1, 
+      });
+      
+      gsap.to('.english', {
+        duration: 0.5,
+        opacity: 0,
+        x: '10%', 
+        ease: "power3.out",
+        delay: 2,
+      });
+
+      // Hindi Text
+
+      gsap.from(".hindi", {
+        duration: 0.5,
+        opacity: 0,
+        x: '-20%',
+        ease: "power3.out",
+      });
+      
+      gsap.to('.hindi', {
+        duration: 0.5,
+        opacity: 1,
+        x: 0, 
+        ease: "power3.out",
+        delay: 2.1, 
+      });
+      
+      gsap.to('.hindi', {
+        duration: 0.5,
+        opacity: 0,
+        x: '10%', 
+        ease: "power3.out",
+        delay: 3.2,
+      });
+
+      // Tamil Text
+
+      gsap.from(".tamil", {
+        duration: 0.5,
+        opacity: 0,
+        x: '-20%',
+        ease: "power3.out",
+      });
+      
+      gsap.to('.tamil', {
+        duration: 0.5,
+        opacity: 4,
+        x: 0, 
+        ease: "power3.out",
+        delay: 3.2, 
+      });
+      
+      gsap.to('.tamil', {
+        duration: 0.5,
+        opacity: 0,
+        x: '10%', 
+        ease: "power3.out",
+        delay: 4.2,
+      });
+
+      // Punjabi Text
+
+      gsap.from(".punjabi", {
+        duration: 0.5,
+        opacity: 0,
+        x: '-20%',
+        ease: "power3.out",
+      });
+      
+      gsap.to('.punjabi', {
+        duration: 0.5,
+        opacity: 1,
+        x: 0, 
+        ease: "power3.out",
+        delay: 4.4, 
+      });
+      
+      gsap.to('.punjabi', {
+        duration: 0.5,
+        opacity: 0,
+        x: '10%', 
+        ease: "power3.out",
+        delay: 5.4,
+      });
+
+      // Marathi Text
+
+      gsap.from(".marathi", {
+        duration: 0.5,
+        opacity: 0,
+        x: '-20%',
+        ease: "power3.out",
+      });
+      
+      gsap.to('.marathi', {
+        duration: 0.5,
+        opacity: 1,
+        x: 0, 
+        ease: "power3.out",
+        delay: 5.5, 
+      });
+      
+      gsap.to('.marathi', {
+        duration: 0.5,
+        opacity: 0,
+        x: '10%', 
+        ease: "power3.out",
+        delay: 6.5,
+      });
+
+      gsap.to('.main', {delay: 6.9, duration: 1.8, y: '-100%', scale: 0.959, borderBottomLeftRadius: '7vw', borderBottomRightRadius: '7vw', ease: "expo.out"});
+      }, []);
 
 
 
   return (
-    
-    <div className='mainBody'>
+     
+      <div className='mainBody'>
 
-      <div className='main'>
+        <div className='main'>
 
-        <h1 className='name'> Sanket Sonawane's</h1>
+          <h1 className='name'> Sanket Sonawane's</h1>
 
-        <h2 className='subText'> Portfolio </h2>
+          <h2 className='subText'> Portfolio </h2>
 
-        <div className='greet'>
+          <div className='greet'>
 
-          <h4 className='english'> Hello </h4>
-          <h4 className='hindi'> नमस्ते </h4>
-          <h4 className='tamil'> வணக்கம் </h4>
-          <h4 className='punjabi'> ਸਤਿ ਸ੍ਰੀ ਅਕਾਲ </h4>
-          <h4 className='marathi'> नमस्कार </h4>
+            <h4 className='english'> Hello </h4>
+            <h4 className='hindi'> नमस्ते </h4>
+            <h4 className='tamil'> வணக்கம் </h4>
+            <h4 className='punjabi'> ਸਤਿ ਸ੍ਰੀ ਅਕਾਲ </h4>
+            <h4 className='marathi'> नमस्कार </h4>
+
+          </div>
 
         </div>
 
+        <div className='firstInfo'>
+
+          <Navbar />
+
+        </div>
+
+
+
       </div>
-
-      <div className='firstInfo'>
-
-         <Navbar />
-
-      </div>
-
-
-
-    </div>
-
+    
   )
 }
 
