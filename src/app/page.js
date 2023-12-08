2fd63c315a4ff28be9bebfb63465a743f9f568c0
SanketSonawane11 <sanketsonawane424@gmail.com>
2023-12-08 15:10:04 +0530

    Added animations

--- src/app/page.js
+++ src/app/page.js
@@ -13,7 +13,18 @@ function About() {
   };
 
   useEffect(() => {
-    gsap.to('.main', {delay: 1, duration: 1.8, y: '-100%', scale: 0.959, borderBottomLeftRadius: '4vw', borderBottomRightRadius: '4vw', ease: "expo.out"});
+    // const t = gsap.timeline();
+
+    // t
+      gsap.from(".name", {duration: 1, opacity: 0, y: '-100%' })
+
+      gsap.to('.name', {duration: 1, opacity: 1, y: 0, ease: "back.out(1.7)" })
+
+      gsap.from(".subText", {duration: 1, opacity: 0, y: '-100%'})
+
+      gsap.to('.subText', {duration: 1, opacity: 1, y: 0, ease: "back.out(1.7)"})
+
+      gsap.to('.main', {delay: 2.3, duration: 1.8, y: '-100%', scale: 0.959, borderBottomLeftRadius: '4vw', borderBottomRightRadius: '4vw', ease: "expo.out"});
       }, []);
 
 
@@ -30,7 +41,7 @@ function About() {
 
       </div>
 
-      
+
 
     </div>
 
